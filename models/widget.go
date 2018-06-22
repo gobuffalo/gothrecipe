7d5ffe8e2e902e421eb0dcad543c6d0a88d8dc98
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-22 12:07:35 +1000

    upgrade buffalo v0.12.1

--- models/widget.go
+++ models/widget.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
-	"github.com/markbates/validate"
-	"github.com/markbates/validate/validators"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
 )
 
 type Widget struct {
